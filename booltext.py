b4c927e833171a0adeea384e9a89163e361eb1f8
oliverkolbet <oliver.kolbet@gmail.com>
2020-08-01 11:58:32 -0700

    modified:   booltext.py

--- booltext.py
+++ booltext.py
@@ -10,7 +10,16 @@ colors={
 'ORANGE':'\033[33m',
 'PURPLE':'\033[35m'
 }
-
+info=['8','8','8','8']
+def indone(junk):
+	kdone=0
+	for qwerty in junk:
+		if '<' in qwerty:
+			kdone+=1
+	if kdone < 1:
+		return 0
+	else:
+		return 1
 mode = 'auto'
 if mode == 'man':
 	print('Choose a .btxt file:')
@@ -32,95 +41,113 @@ info = info.split('\n')
 for line in info:
 	if line == '':
 		info.remove('')
-for k in info:
-	if '<CLEAR>' in info:
-		os.system('clear')
-		info.remove('<CLEAR>')
-	if '<PINK>' in info:
-		print('\033[95m',end='')
-		info.remove('<PINK>')
-	elif '<COLOR' in k:
-		col=k.split('%')
-		color=col[1]
-		if color in colors:
-			print(colors[color],end='')
-		del info[info.index(col)]
-	if '<DELAY>' in info:
-		delay=True
-		info.remove('<DELAY>')
-	else:
-		delay=False
-	if '<$SHIFT>' in info:
-		bangkey = 'and'
-		andkey = 'or'
-		orkey = 'not'
-		info.remove('<$SHIFT>')
-	elif '<$EXCLAMATION>' in info:
-		bangkey = 'UH OH!'
-		andkey = 'AND...'
-		orkey = 'OR?!'
-		info.remove('<$EXCLAMATION>')
-	elif '<$KEYS' in k:
-		key=k.split('%')
-		bangkey=keys[1]
-		andkey=keys[2]
-		orkey=keys[3]
-		del info[info.index(key)]
-	else:
-		bangkey='not'
-		andkey='and'
-		orkey='or'
-	if '<SEPARATOR' in k:
-		se=k.split('%')
-		bkey=se[1]
-		del info[info.index(se)]
-	else:
-		bkey='/'
-	if '<C' in k:
-		comment=k.split('%')
-		if comment[1] == '>':
-			print(comment[2])
-		elif comment[1] == '*':
-			print(comment[2])
-			file=open(bfile, 'r')
-			finfo = file.read().strip()
-			finfo=finfo.split('\n')
-			finfo[finfo.index(k)]=''
-			file=open(bfile,'w')
-			file.writelines('\n'.join(finfo))
-			if len(comment) > 3:
-				if comment[3] == '$':
-					os.system(comment[1])
-		elif comment[1] == '$':
-			os.system(comment[1])
-		info.remove(k)
-	if '<LINK' in k:
-		lkey=k.split('%')
-		def com(arg):
-			os.system('echo \"'+arg+'\" > .tmp_btxt && python3 \"$(cat ~/.btxt_locale)\"')
-		comm=True
-		info.remove(k)
-	else:
-		comm=False
-	if '<COMMAND' in k:
-		cm=k.split('%')
-		os.system(cm[1])
-		info.remove(k)
-	if '<OUTPUT>' in k:
-		ouu=k.split('%')
-		info.remove(k)
-		ofile=ouu[1]
-		fo = '\n'.join(info)
-		os.system('echo \"'+fo+'\" >> '+ofile)
-	else:
-		output = False
-	for line in info:
-		line=line.split(',')
-	if '<SHOW>' in info:
-		print('---------TEXT:--------')
-		info.remove('<SHOW>')
-		print(info)
-		print('----------------------')
+lkey=''
+btxtdone=False
+identt=False
+while indone(info) == True:
+	identt=True
+	for k in info:
+		if btxtdone == False:
+			if '<BTXT>' in k:
+				info.remove(k)
+				btxtdone=True
+			else:
+				if btxtdone == False:
+					print('No BTXT identifier found in file')
+		if '<CLEAR>' == k:
+			os.system('clear')
+			info.remove('<CLEAR>')
+		if '<PINK>' == k:
+			print('\033[95m',end='')
+			info.remove('<PINK>')
+		if '<COLOR' in k:
+			col=k.split(':')
+			color=col[1]
+			if color in colors:
+				print(colors[color],end='')
+			del info[info.index(k)]
+		if '<DELAY>' == k:
+			delay=True
+			info.remove('<DELAY>')
+		else:
+			delay=False
+		if '<$SHIFT>' == k:
+			bangkey = 'and'
+			andkey = 'or'
+			orkey = 'not'
+			info.remove('<$SHIFT>')
+		elif '<$EXCLAMATION>' == k:
+			bangkey = 'UH OH!'
+			andkey = 'AND...'
+			orkey = 'OR?!'
+			info.remove('<$EXCLAMATION>')
+		elif '<$KEYS' in k:
+			key=k.split(':')
+			bangkey=keys[1]
+			andkey=keys[2]
+			orkey=keys[3]
+			del info[info.index(key)]
+		else:
+			bangkey='not'
+			andkey='and'
+			orkey='or'
+		if '<SEPARATOR' in k:
+			se=k.split(':')
+			bkey=se[1]
+			del info[info.index(se)]
+		else:
+			bkey='/'
+		if '<C' in k:
+			comment=k.split(':')
+			if comment[1] == '>':
+				print(comment[2])
+			elif comment[1] == '*':
+				print(comment[2])
+				file=open(bfile, 'r')
+				finfo = file.read().strip()
+				finfo=finfo.split('\n')
+				finfo[finfo.index(k)]=''
+				file=open(bfile,'w')
+				file.writelines('\n'.join(finfo))
+				if len(comment) > 3:
+					if comment[3] == '$':
+						os.system(comment[1])
+			elif comment[1] == '$':
+				os.system(comment[1])
+			del info[info.index(k)]
+		if '<LINK' in k:
+			lkey=info.index(k)
+			def com(arg):
+				os.system('echo \"'+arg+'\" > .tmp_btxt && python3 \"$(cat ~/.btxt_locale)\"')
+			finfo=[]
+			for oofy in info:
+				finfo.append(oofy)
+			comm=True
+			info.remove(k)
+		else:
+			comm=False
+		if '<COMMAND' in k:
+			cm=k.split(':')
+			os.system(cm[1])
+			info.remove(k)
+		if '<OUTPUT' in k:
+			ouu=k.split(':')
+			info.remove(k)
+			ofile=ouu[1]
+			fo = '\n'.join(info)
+			os.system('echo \"'+fo+'\" >> '+ofile)
+		else:
+				output = False
+		if '<SHOW>' == k:
+			print('---------TEXT:--------')
+			info.remove('<SHOW>')
+			print(info)
+			print('----------------------')
+if identt == False:
+	print("No BTXT identifier found in file.")
+	exit()
+for line in info:
+	line=line.split(',')
 i=0
 while i < (len(info)):
 	e=0
@@ -151,5 +178,5 @@ while i < (len(info)):
 
 	i+=1
 if comm == True:
-	com(lkey[1])
+	com(finfo[lkey])
 
